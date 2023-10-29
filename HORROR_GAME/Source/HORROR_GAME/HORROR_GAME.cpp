3805bc473522ba72267e1fef3c20a9905dc31654
Sean Nichols <78358189+OGreatHippo@users.noreply.github.com>
2023-10-29 18:49:57 +0000

    initial commit

---
+++ HORROR_GAME/Source/HORROR_GAME/HORROR_GAME.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HORROR_GAME.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HORROR_GAME, "HORROR_GAME" );
